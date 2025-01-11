92299d51e82af071e7240e56b568a387f9e233ed
Jacob Garland <352cjg@gmail.com>
2025-01-11 13:17:11 -0500

    random edits-jacob

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import LoginPage from "./pages/login-page";
 import RegisterPage from "./pages/register";
-import Homepage from "./pages/homepage";
 import Profile from "./pages/dashboard";
 
 const App: React.FC = () => {
@@ -10,7 +9,6 @@ const App: React.FC = () => {
     <Router>
       <Routes>
         <Route path="/" element={<Navigate to="/homepage" />} />
-        <Route path="/homepage" element={<Homepage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/profile" element={<Profile />} />
--- client/src/pages/homepage.tsx
+++ client/src/pages/homepage.tsx
@@ -4,8 +4,7 @@ export default function Homepage() {
   return (
     // TODO: BUILD AND ADD BULMA CSS TO MAKE IT LOOK BETTER
     <div>
-      <h1>Homepage</h1>
-      <p>Welcome to the homepage</p>
+      <h1>EngineBase</h1>
     </div>
   );
 }
