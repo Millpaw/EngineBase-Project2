72237bde2158036650117e79ade5f8587866476a
Jacob Garland <352cjg@gmail.com>
2025-01-13 17:56:44 -0500

    edited server.js - jacob

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import sequelize from '../../server/src/config/database';
+import sequelize from './config/database';
 
 const app = express();
 app.use(express.json());
@@ -9,6 +9,6 @@ sequelize.authenticate()
     console.log('✅ Database connection successful!');
     app.listen(5000, () => console.log('🚀 Server running on http://localhost:5000'));
   })
-  .catch((error: unknown) => {
+  .catch((error) => {
     console.error('❌ Unable to connect to the database:', error);
   });
